d1db43c0e9f765e3c0574c36d4dbb9ef44dcee52
xjb1337 <1270744734@qq.com>
2026-09-12 15:50:32 +0800

    update: 计划起点改为 2026-09-14（下周一开营）

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='kaoyan-v1';
+const C='kaoyan-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
